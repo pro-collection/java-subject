85d3689a8cd01b0efea7dd4fe0c85055897b123f
yanlele <331393627@qq.com>
2020-05-30 21:23:48 +0800

    feat: 添加测试

---
+++ hadoop-demo/src/test/java/com/yanle/hdfs/HDFSApplication.java
@@ -0,0 +1,37 @@
+package com.yanle.hdfs;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.net.URI;
+
+
+public class HDFSApplication {
+    public static final String HDFS_PATH = "hdfs://66.66.66.67:8020";
+
+    FileSystem fileSystem = null;
+    Configuration configuration = null;
+
+    @Test
+    public void makeDir() throws Exception {
+        fileSystem.mkdirs(new Path("/test"));
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        System.out.println("settUp");
+        configuration = new Configuration();
+        fileSystem = FileSystem.get(new URI(HDFS_PATH), configuration);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        configuration = null;
+        fileSystem = null;
+        System.out.println(" tearDown ");
+    }
+}
