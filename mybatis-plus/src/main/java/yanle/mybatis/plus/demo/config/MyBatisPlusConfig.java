10899eb5a050c3de10dda471e3665521aa9d787e
yanlele <331393627@qq.com>
2020-06-27 19:08:01 +0800

    feat: 修改配置

---
+++ mybatis-plus/src/main/java/yanle/mybatis/plus/demo/config/MyBatisPlusConfig.java
@@ -0,0 +1,9 @@
+package yanle.mybatis.plus.demo.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("yanle.mybatis.plus.demo.mapper")
+public class MyBatisPlusConfig {
+}
