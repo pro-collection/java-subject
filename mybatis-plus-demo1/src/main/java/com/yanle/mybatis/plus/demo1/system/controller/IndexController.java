bf400d4a7378063b965aa623deae21aca088d4c9
yanlele <331393627@qq.com>
2020-07-11 15:32:47 +0800

    feat: 修改静态资源路径文件

--- mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/controller/IndexController.java
+++ mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/controller/IndexController.java
@@ -1,20 +1,46 @@
 package com.yanle.mybatis.plus.demo1.system.controller;
 
 import com.yanle.mybatis.plus.demo1.common.base.Status;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/web")
 public class IndexController {
 
-    @RequestMapping("")
+    @RequestMapping("/home")
     public String home() {
         return "home";
     }
 
-    @RequestMapping("*")
-    public String redirect() {
-        return "home";
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping("/introduce")
+    public String introduce() {
+        return "introduce";
+    }
+
+    @RequestMapping("/admin")
+    @ResponseBody
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public String admin(){
+        return "是管理员";
+    }
+
+    @RequestMapping("/user")
+    @ResponseBody
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public String user(){
+        return "是普通用户";
     }
 }
