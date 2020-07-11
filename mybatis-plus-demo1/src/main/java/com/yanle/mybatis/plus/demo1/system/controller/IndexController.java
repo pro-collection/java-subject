21025be01d6044ef498b4a03439c8c8816fb2bda
yanlele <331393627@qq.com>
2020-07-11 14:04:21 +0800

    feat: 添加页面路由导航

--- mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/controller/IndexController.java
+++ mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/controller/IndexController.java
@@ -1,13 +1,20 @@
 package com.yanle.mybatis.plus.demo1.system.controller;
 
+import com.yanle.mybatis.plus.demo1.common.base.Status;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/web")
 public class IndexController {
 
-    @RequestMapping("/")
+    @RequestMapping("")
     public String home() {
+        return "home";
+    }
+
+    @RequestMapping("*")
+    public String redirect() {
         return "home";
     }
 }
