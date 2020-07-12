afa8ceb70b7628a4f157bd8b422daab4a324bd91
yanlele <331393627@qq.com>
2020-07-12 22:02:58 +0800

    feat: 接口定义完成

--- mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/service/SysMenuService.java
+++ mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/service/SysMenuService.java
@@ -40,5 +40,55 @@ public interface SysMenuService {
      */
     SysMenu getByName(String menuName, String menuCode, String menuHref);
 
+    /**
+     * 根据id查询菜单
+     * @param id id
+     * @return return
+     */
+    SysMenu getById(String id);
 
+    /**
+     * 获取一级菜单
+     * @return 一级菜单
+     */
+    List<SysMenu> getFirstMenu();
+
+    /**
+     * 获取二级菜单
+     */
+    List<SysMenu> getSecondMenu();
+
+    /**
+     * 根据角色id查询所有菜单
+     * @param roleId 角色id
+     * @return 菜单
+     */
+    List<String> getRoleMenu(String roleId);
+
+    /**
+     * 获取菜单层级
+     * @return 菜单登记
+     */
+    List<String> getMenuLevel();
+
+    /**
+     * 查询当前菜单的上级菜单
+     * @param menuLevel 上级菜单层级
+     * @return 上级菜单名称
+     */
+    List<MenuNameVO> getPreviousMenu(String menuLevel);
+
+    /**
+     * 根据菜单名称查询菜单id
+     * @param menuNames 菜单名称
+     * @return 菜单id
+     */
+    String getByMenuName(String menuNames);
+
+    /**
+     * 根据id删除菜单
+     * @param id id
+     * @return 返回值
+     */
+    int deleteMenuById(String id);
 }
