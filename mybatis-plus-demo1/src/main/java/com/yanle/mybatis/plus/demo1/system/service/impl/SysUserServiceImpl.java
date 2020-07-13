d8968f29695fe00b18c3d6c13715b8d620a2ebfa
yanlele <331393627@qq.com>
2020-07-13 23:21:00 +0800

    feat: SysUserServiceImpl

---
+++ mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/service/impl/SysUserServiceImpl.java
@@ -0,0 +1,43 @@
+package com.yanle.mybatis.plus.demo1.system.service.impl;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.yanle.mybatis.plus.demo1.system.entity.SysUser;
+import com.yanle.mybatis.plus.demo1.system.service.SysUserService;
+import sun.jvm.hotspot.debugger.Page;
+
+public class SysUserServiceImpl implements SysUserService {
+    @Override
+    public SysUser findByName(String name) {
+        return null;
+    }
+
+    @Override
+    public IPage<SysUser> getAll(Page page) {
+        return null;
+    }
+
+    @Override
+    public SysUser getById(String id) {
+        return null;
+    }
+
+    @Override
+    public int deleteById(String id) {
+        return 0;
+    }
+
+    @Override
+    public int updateById(SysUser sysUser) {
+        return 0;
+    }
+
+    @Override
+    public int insert(SysUser sysUser) {
+        return 0;
+    }
+
+    @Override
+    public int updatePasswordById(String password, String id) {
+        return 0;
+    }
+}
