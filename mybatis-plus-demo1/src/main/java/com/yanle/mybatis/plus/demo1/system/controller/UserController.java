125349ee9ca487b9776e923fe61cecf93edf5ec1
yanlele <331393627@qq.com>
2020-07-15 07:46:43 +0800

    feat: UserController

--- mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/controller/UserController.java
+++ mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/controller/UserController.java
@@ -1,10 +1,19 @@
 package com.yanle.mybatis.plus.demo1.system.controller;
 
+import com.yanle.mybatis.plus.demo1.system.service.SysRoleService;
+import com.yanle.mybatis.plus.demo1.system.service.SysUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/user")
+@RequestMapping("web/user")
 public class UserController {
+    @Autowired
+    private SysUserService sysUserService;
+
+    @Autowired
+    private SysRoleService sysRoleService;
+
 
 }
