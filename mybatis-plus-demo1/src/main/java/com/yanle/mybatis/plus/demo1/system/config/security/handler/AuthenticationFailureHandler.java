89ad46198deb7d849c561b6a54df8645fd4e4358
yanlele <331393627@qq.com>
2020-07-15 23:51:48 +0800

    feat: AuthenticationFailureHandler

---
+++ mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/config/security/handler/AuthenticationFailureHandler.java
@@ -0,0 +1,6 @@
+package com.yanle.mybatis.plus.demo1.system.config.security.handler;
+
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+public class AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+}
