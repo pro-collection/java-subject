1f8e9d58408d04de9e78fb0da8f31a58055a598e
yanlele <331393627@qq.com>
2020-07-15 09:44:44 +0800

    feat: 写入缓存方法

--- mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/common/utils/RedisUtils.java
+++ mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/common/utils/RedisUtils.java
@@ -6,6 +6,7 @@ import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class RedisUtils {
@@ -15,15 +16,37 @@ public class RedisUtils {
 
     /**
      * 写入缓存
-     * @param key key
+     * @param key   key
      * @param value value
      * @return boolean
      */
+    @SuppressWarnings("unchecked")
     public boolean set(String key, Object value) {
         boolean result = false;
         try {
             ValueOperations<Serializable, Object> operations = redisTemplate.opsForValue();
             operations.set(key, value);
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * 写入缓存
+     * @param key key
+     * @param value value
+     * @param expireTime 失效时间
+     * @param timeUnit 失效时间单位
+     * @return return
+     */
+    @SuppressWarnings("unchecked")
+    public boolean set(String key, Object value, Long expireTime, TimeUnit timeUnit) {
+        boolean result = false;
+        try {
+            redisTemplate.opsForValue().set(key, value);
+            redisTemplate.expire(key, expireTime, timeUnit);
             result = true;
         } catch (Exception e) {
             e.printStackTrace();
