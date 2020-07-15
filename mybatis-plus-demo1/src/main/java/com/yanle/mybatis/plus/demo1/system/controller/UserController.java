8a1bfc5c08034f7d942fdd8e70cf615f7abf159b
yanlele <331393627@qq.com>
2020-07-15 08:17:54 +0800

    feat: 用户相关路由

--- mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/controller/UserController.java
+++ mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.yanle.mybatis.plus.demo1.system.controller;
 
+import com.yanle.mybatis.plus.demo1.system.entity.SysUser;
 import com.yanle.mybatis.plus.demo1.system.service.SysRoleService;
 import com.yanle.mybatis.plus.demo1.system.service.SysUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,5 +18,27 @@ public class UserController {
     @Autowired
     private SysRoleService sysRoleService;
 
+    @GetMapping("/list")
+    public String list() {
+        return "module/user/user";
+    }
 
+    @GetMapping("/update")
+    public String update(String id, Model model) {
+        SysUser sysUser = sysUserService.getById(id);
+        String roleName = sysRoleService.getRoleNameById(sysUser.getId());
+        model.addAttribute("sysUser", sysUser);
+        model.addAttribute("roleName", roleName);
+        return "module/user/updateUser";
+    }
+
+    @GetMapping("/add")
+    public String add() {
+        return "module/user/addUser";
+    }
+
+    @GetMapping("/changePassword")
+    public String changePassword() {
+        return "module/user/changePassword";
+    }
 }
