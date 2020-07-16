615f2f4d0cbfbcea377a272cd074f9eee8f1f138
yanlele <331393627@qq.com>
2020-07-16 23:03:36 +0800

    feat: 修改

--- mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/config/security/handler/AuthenticationFailureHandler.java
+++ mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/config/security/handler/AuthenticationFailureHandler.java
@@ -1,6 +1,11 @@
 package com.yanle.mybatis.plus.demo1.system.config.security.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
 
+@Component
+@Slf4j
 public class AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
 }
