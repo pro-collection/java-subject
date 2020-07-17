1dcba89489ca7e7f36f3f78a697468e8008e4eec
yanlele <331393627@qq.com>
2020-07-17 21:16:08 +0800

    feat: SysLogService

--- mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/service/SysLogService.java
+++ mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/service/SysLogService.java
@@ -1,10 +1,13 @@
 package com.yanle.mybatis.plus.demo1.system.service;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.yanle.mybatis.plus.demo1.system.entity.SysLog;
 
 import javax.servlet.http.HttpServletRequest;
 
 public interface SysLogService {
     int saveLoginLog(HttpServletRequest request, String message, String name);
 
+    IPage<SysLog> findSysLogPage(Page page);
 }
