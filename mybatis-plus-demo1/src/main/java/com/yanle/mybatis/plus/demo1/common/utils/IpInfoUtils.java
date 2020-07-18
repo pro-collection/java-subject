3be26c9f5d86d3a9119a004d101c473088d823f8
yanlele <331393627@qq.com>
2020-07-18 21:38:45 +0800

    feat: getIpAddress

--- mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/common/utils/IpInfoUtils.java
+++ mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/common/utils/IpInfoUtils.java
@@ -1,6 +1,10 @@
 package com.yanle.mybatis.plus.demo1.common.utils;
 
+import com.yanle.mybatis.plus.demo1.common.base.Constants;
+
 import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class IpInfoUtils {
 
@@ -15,6 +19,28 @@ public class IpInfoUtils {
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip))
             ip = request.getHeader("Proxy-Client-IP");
         // todo
-        return "";
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip))
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip  = request.getRemoteAddr();
+            if (Constants.LOCAL_HOST.equals(ip)) {
+                InetAddress inet = null;
+                try {
+                    inet = InetAddress.getLocalHost();
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                }
+                ip = inet.getHostAddress();
+            }
+        }
+        if (ip != null && ip.length()>15) {
+            if (ip.indexOf(",")>0) {
+                ip = ip.substring(0, ip.indexOf(","));
+            }
+        }
+        if ("0:0:0:0:0:0:0:1".equals(ip)) {
+            ip = Constants.LOCAL_HOST;
+        }
+        return ip;
     }
 }
