8500fb4809ad00e79a0fdb9aa85b4fdca0acaebf
yanlele <331393627@qq.com>
2020-07-19 22:19:34 +0800

    feat: ValidateCodeFilter

--- mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/config/filter/ValidateCodeFilter.java
+++ mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/config/filter/ValidateCodeFilter.java
@@ -1,4 +1,59 @@
 package com.yanle.mybatis.plus.demo1.system.config.filter;
 
-public class ValidateCodeFilter {
+import com.yanle.mybatis.plus.demo1.common.base.Constants;
+import com.yanle.mybatis.plus.demo1.system.config.exception.ValidateCodeException;
+import com.yanle.mybatis.plus.demo1.system.config.security.handler.AuthenticationFailureHandler;
+import com.yanle.mybatis.plus.demo1.system.service.RedisService;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.util.PathMatcher;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class ValidateCodeFilter extends OncePerRequestFilter {
+    private final RedisService redisService;
+
+    private final AuthenticationFailureHandler authenticationFailureHandler;
+
+    private static final PathMatcher PATHMATCHER = new AntPathMatcher();
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (Constants.REQUEST_MODE_POST.equals(request.getMethod()) && PATHMATCHER.match(Constants.LOGIN_URL,request.getServletPath())){
+            try {
+                codeValidate(request);
+            } catch (ValidateCodeException e){
+                //验证码不通过，跳到错误处理器处理
+                authenticationFailureHandler.onAuthenticationFailure(request,response,e);
+                return;
+            }
+        }
+        doFilter(request,response,filterChain);
+    }
+
+    private void codeValidate(HttpServletRequest request) throws ValidateCodeException {
+        String code = request.getParameter("code");
+        String uuidCode = request.getParameter("uuidCode");
+        if (org.apache.commons.lang3.StringUtils.isEmpty(code)){
+            throw new ValidateCodeException("验证码的值不能为空");
+        }
+        String codeVal = redisService.getCodeVal(uuidCode);
+        if (org.apache.commons.lang3.StringUtils.isEmpty(codeVal)) {
+            throw new ValidateCodeException("验证码已过期");
+        }
+        if (!StringUtils.equals(codeVal,code)) {
+            throw new ValidateCodeException("验证码不匹配");
+        }
+    }
 }
