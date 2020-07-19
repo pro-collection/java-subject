8fc081baaf2502e4420f6d798e5bb296651b6733
yanlele <331393627@qq.com>
2020-07-19 22:00:02 +0800

    feat: SecurityMvcConfigurer

--- mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/config/security/SecurityMvcConfigurer.java
+++ mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/config/security/SecurityMvcConfigurer.java
@@ -1,5 +1,6 @@
 package com.yanle.mybatis.plus.demo1.system.config.security;
 
+import com.yanle.mybatis.plus.demo1.system.config.interceptor.AccessAuthHandlerInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -8,15 +9,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class SecurityMvcConfigurer implements WebMvcConfigurer {
 
+    private AccessAuthHandlerInterceptor accessAuthHandlerInterceptor;
 
+    public SecurityMvcConfigurer(AccessAuthHandlerInterceptor accessAuthHandlerInterceptor) {
+        this.accessAuthHandlerInterceptor = accessAuthHandlerInterceptor;
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-
+        registry
+                .addInterceptor(accessAuthHandlerInterceptor)
+                .excludePathPatterns("/static/**")
+                .addPathPatterns("/**");
     }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-
+        registry.addViewController("/login").setViewName("login.html");
+        registry.addViewController("/403").setViewName("403.html");
     }
 }
