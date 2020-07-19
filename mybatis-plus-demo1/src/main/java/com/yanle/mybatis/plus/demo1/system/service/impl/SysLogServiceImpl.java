b7bac5bb6a2d7caaeb6adac434a70b719eeac2c7
yanlele <331393627@qq.com>
2020-07-19 17:49:56 +0800

    feat: saveLoginLog

--- mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/service/impl/SysLogServiceImpl.java
+++ mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/service/impl/SysLogServiceImpl.java
@@ -2,6 +2,8 @@ package com.yanle.mybatis.plus.demo1.system.service.impl;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.yanle.mybatis.plus.demo1.common.utils.IpInfoUtils;
+import com.yanle.mybatis.plus.demo1.common.utils.UUIDUtils;
 import com.yanle.mybatis.plus.demo1.system.dao.SysLogDao;
 import com.yanle.mybatis.plus.demo1.system.entity.SysLog;
 import com.yanle.mybatis.plus.demo1.system.service.SysLogService;
@@ -10,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 
 @Service
 @Slf4j
@@ -19,6 +22,26 @@ public class SysLogServiceImpl implements SysLogService {
 
     @Override
     public int saveLoginLog(HttpServletRequest request, String message, String name) {
+        try {
+            String ipAddr = IpInfoUtils.getIpAddress(request);
+            String ipSource = IpInfoUtils.getIpSource(ipAddr);
+            String browser = IpInfoUtils.getBrowser(request);
+            String systemName = IpInfoUtils.getSystemName(request);
+            SysLog sysLog = SysLog.builder()
+                    .username(name)
+                    .browserName(browser)
+                    .createDate(new Date())
+                    .id(UUIDUtils.getUUID())
+                    .ipAddress(ipAddr)
+                    .ipSource(ipSource)
+                    .message(message)
+                    .systemName(systemName)
+                    .build();
+            return sysLogDao.insert(sysLog);
+        } catch (Exception e) {
+            log.error("获取ip来源出错");
+            e.printStackTrace();
+        }
         return 0;
     }
 
