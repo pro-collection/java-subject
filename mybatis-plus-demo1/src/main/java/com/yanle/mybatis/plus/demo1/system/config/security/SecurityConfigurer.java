cfaff845884bf023527d34de7358f798961dff31
yanlele <331393627@qq.com>
2020-07-22 23:46:40 +0800

    feat: 添加SecurityConfigurer

--- mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/config/security/SecurityConfigurer.java
+++ mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/config/security/SecurityConfigurer.java
@@ -1,15 +1,21 @@
 package com.yanle.mybatis.plus.demo1.system.config.security;
 
+import com.yanle.mybatis.plus.demo1.common.base.Constants;
+import com.yanle.mybatis.plus.demo1.system.config.filter.ValidateCodeFilter;
 import com.yanle.mybatis.plus.demo1.system.config.security.handler.AuthenticationFailureHandler;
 import com.yanle.mybatis.plus.demo1.system.config.security.handler.AuthenticationSuccessHandler;
+import com.yanle.mybatis.plus.demo1.system.config.security.handler.CustomLogoutSuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -34,7 +40,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     private final CustomAuthenticationProvider customAuthenticationProvider;
 
     // todo
-//    private final  ValidateCodeFilter validateCodeFilter;
+    private final ValidateCodeFilter validateCodeFilter;
 
     private final CustomInvalidSessionStrategy customInvalidSessionStrategy;
 
@@ -44,8 +50,46 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     private final AuthenticationFailureHandler authenticationFailureHandler;
 
+    @Autowired
+    private SessionRegistry sessionRegistry;
+
+    @Autowired
+    private CustomLogoutSuccessHandler customLogoutSuccessHandler;
+
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.headers()
+                .frameOptions()
+                .disable();
+
+        http.addFilterBefore(validateCodeFilter, UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests()
+                .antMatchers("/static/**", "/favicon.ico", "/actuator/**", "/code", "/invalid_session", "/expired", "/logout", "/403").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling()
+                .accessDeniedPage("/403")
+                .and()
+                // form 登录
+                .formLogin()
+                .loginProcessingUrl(Constants.LOGIN_URL)
+                .loginPage(Constants.LOGIN_URL)
+                .permitAll()
+                .and()
+                .csrf().disable()
+                .cors()
+                .and()
+                // 登出
+                .logout()
+                .logoutUrl(Constants.LOGOUT_URL)
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+                .logoutSuccessHandler()
+
     }
 }
