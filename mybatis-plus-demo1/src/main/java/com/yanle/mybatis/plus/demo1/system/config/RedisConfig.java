96b47a363a5b3ce6235b44f6f46bf778cc030d12
yanlele <331393627@qq.com>
2020-07-23 23:25:29 +0800

    feat: RedisConfig

--- mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/config/RedisConfig.java
+++ mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/config/RedisConfig.java
@@ -3,6 +3,7 @@ package com.yanle.mybatis.plus.demo1.system.config;
 import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
@@ -10,11 +11,12 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import java.time.Duration;
-import java.util.Objects;
 
 @Slf4j
 @Configuration
@@ -24,8 +26,8 @@ import java.util.Objects;
 public class RedisConfig extends CachingConfigurerSupport {
 
     /**
-     *  设置 redis 数据默认过期时间，默认6小时
-     *  设置@cacheable 序列化方式
+     * 设置 redis 数据默认过期时间，默认6小时
+     * 设置@cacheable 序列化方式
      */
     @Bean
     public RedisCacheConfiguration redisCacheConfiguration() {
@@ -33,5 +35,12 @@ public class RedisConfig extends CachingConfigurerSupport {
         RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig();
         configuration = configuration.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(fastJsonRedisSerializer)).entryTtl(Duration.ofHours(6));
         return configuration;
+    }
+
+    @Bean(name = "redisTemplate")
+    @ConditionalOnMissingBean(name = "redisTemplate")
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        // todo
     }
 }
