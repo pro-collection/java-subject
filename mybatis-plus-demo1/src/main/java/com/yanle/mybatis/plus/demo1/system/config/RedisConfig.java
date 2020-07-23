61e76569d50b0bc7a58618482ffb7d3f822514e7
yanlele <331393627@qq.com>
2020-07-23 23:13:25 +0800

    feat: RedisCacheConfiguration

--- mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/config/RedisConfig.java
+++ mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/config/RedisConfig.java
@@ -1,13 +1,20 @@
 package com.yanle.mybatis.plus.demo1.system.config;
 
+import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+import java.util.Objects;
 
 @Slf4j
 @Configuration
@@ -15,4 +22,16 @@ import org.springframework.data.redis.core.RedisOperations;
 @ConditionalOnClass(RedisOperations.class)
 @EnableConfigurationProperties(RedisProperties.class)
 public class RedisConfig extends CachingConfigurerSupport {
+
+    /**
+     *  设置 redis 数据默认过期时间，默认6小时
+     *  设置@cacheable 序列化方式
+     */
+    @Bean
+    public RedisCacheConfiguration redisCacheConfiguration() {
+        FastJsonRedisSerializer<Object> fastJsonRedisSerializer = new FastJsonRedisSerializer<>(Object.class);
+        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig();
+        configuration = configuration.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(fastJsonRedisSerializer)).entryTtl(Duration.ofHours(6));
+        return configuration;
+    }
 }
