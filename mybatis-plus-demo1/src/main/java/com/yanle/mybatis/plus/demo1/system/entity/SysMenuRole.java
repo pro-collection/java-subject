c2aecbbe33b3c00beecc78bb52a1ac2f6febbbbd
yanlele <331393627@qq.com>
2020-07-27 23:51:20 +0800

    feat: SysMenuRole

--- mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/entity/SysMenuRole.java
+++ mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/entity/SysMenuRole.java
@@ -1,4 +1,20 @@
 package com.yanle.mybatis.plus.demo1.system.entity;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class SysMenuRole {
+    private String menuId;
+    private String roleId;
+
+    public SysMenuRole(String menuId, String roleId) {
+        this.menuId = menuId;
+        this.roleId = roleId;
+    }
+
+    public SysMenuRole(String menuId) {
+        this.menuId = menuId;
+    }
 }
