893bfa7385798c3942787cc3dbfb9db2b4ae02c0
yanlele <331393627@qq.com>
2020-07-28 08:36:43 +0800

    feat: RoleRestController

---
+++ mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/restfulController/RoleRestController.java
@@ -0,0 +1,17 @@
+package com.yanle.mybatis.plus.demo1.system.restfulController;
+
+import com.yanle.mybatis.plus.demo1.system.service.SysMenuService;
+import com.yanle.mybatis.plus.demo1.system.service.SysRoleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/web/rule")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class RoleRestController {
+    private final SysRoleService sysRoleService;
+    private final SysMenuService sysMenuService;
+//    private final SysmenuRoleService sysMenuService;
+}
