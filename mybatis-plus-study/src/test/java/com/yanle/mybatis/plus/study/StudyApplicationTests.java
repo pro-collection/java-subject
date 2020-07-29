1152f0a5d7ac11f7d3cf7a521acf38aa1af80c12
yanlele <331393627@qq.com>
2020-07-30 07:15:38 +0800

    feat: 初始化

---
+++ mybatis-plus-study/src/test/java/com/yanle/mybatis/plus/study/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanle.mybatis.plus.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
