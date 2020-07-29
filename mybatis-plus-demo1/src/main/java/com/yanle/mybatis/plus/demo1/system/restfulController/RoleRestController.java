5a087a821d6b611270d9f400eafbd0290672e13b
yanlele <331393627@qq.com>
2020-07-29 22:19:16 +0800

    feat: RoleRestController

--- mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/restfulController/RoleRestController.java
+++ mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/restfulController/RoleRestController.java
@@ -1,11 +1,18 @@
 package com.yanle.mybatis.plus.demo1.system.restfulController;
 
+import com.alibaba.fastjson.JSONObject;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.yanle.mybatis.plus.demo1.common.base.ApiResponse;
+import com.yanle.mybatis.plus.demo1.system.entity.SysRole;
 import com.yanle.mybatis.plus.demo1.system.service.SysMenuRoleService;
 import com.yanle.mybatis.plus.demo1.system.service.SysMenuService;
 import com.yanle.mybatis.plus.demo1.system.service.SysRoleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,7 +21,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class RoleRestController {
     private final SysRoleService sysRoleService;
     private final SysMenuService sysMenuService;
-    private final SysMenuRoleService sysMenuService;
+    private final SysMenuRoleService sysMenuRoleService;
+
+    @GetMapping("/getRoleInfo")
+    public ApiResponse getRoleInfo(
+            @RequestParam("page") int page,
+            @RequestParam("page_size") int pageSize
+    ) {
+        JSONObject jsonObject = new JSONObject();
+        IPage<SysRole> sysRoleList = sysRoleService.getAll(new Page(page, pageSize));
+        jsonObject.put("total", sysRoleList.getTotal());
+        jsonObject.put("page", sysRoleList.getCurrent());
+        jsonObject.put("page_size", sysRoleList.getSize());
+        jsonObject.put("sysRoleList", sysRoleList.getRecords());
+        return ApiResponse.ofSuccess(jsonObject);
+    }
 
 
 }
