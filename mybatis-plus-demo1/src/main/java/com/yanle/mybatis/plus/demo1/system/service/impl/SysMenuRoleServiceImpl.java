845264e733c6ecd917c4cf2590ffa2680224e334
yanlele <331393627@qq.com>
2020-07-29 21:59:02 +0800

    feat: SysMenuRoleServiceImpl

--- mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/service/impl/SysMenuRoleServiceImpl.java
+++ mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/service/impl/SysMenuRoleServiceImpl.java
@@ -1,4 +1,30 @@
 package com.yanle.mybatis.plus.demo1.system.service.impl;
 
-public class SysMenuRoleServiceImpl {
+import com.yanle.mybatis.plus.demo1.system.entity.SysMenuRole;
+import com.yanle.mybatis.plus.demo1.system.service.SysMenuRoleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class SysMenuRoleServiceImpl implements SysMenuRoleService {
+    private final SysMenuRoleDao sysMenuRoleDao;
+
+    @Override
+    public int addMenuRole(SysMenuRole sysMenuRole) {
+        return 0;
+    }
+
+    @Override
+    public int deleteByRoleId(String roleId) {
+        return 0;
+    }
+
+    @Override
+    public List<String> getAllMenuId(String roleId, List<String> parentIds) {
+        return null;
+    }
 }
