119d0c8962a8c157c14919397c26bc26f5966fd1
yanlele <331393627@qq.com>
2020-07-29 22:13:35 +0800

    fat: SysMenuRoleServiceImpl

--- mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/service/impl/SysMenuRoleServiceImpl.java
+++ mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/service/impl/SysMenuRoleServiceImpl.java
@@ -1,5 +1,7 @@
 package com.yanle.mybatis.plus.demo1.system.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.yanle.mybatis.plus.demo1.system.dao.SysMenuRoleDao;
 import com.yanle.mybatis.plus.demo1.system.entity.SysMenuRole;
 import com.yanle.mybatis.plus.demo1.system.service.SysMenuRoleService;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -15,16 +18,19 @@ public class SysMenuRoleServiceImpl implements SysMenuRoleService {
 
     @Override
     public int addMenuRole(SysMenuRole sysMenuRole) {
-        return 0;
+        return sysMenuRoleDao.addMenuRole(sysMenuRole);
     }
 
     @Override
     public int deleteByRoleId(String roleId) {
-        return 0;
+        return sysMenuRoleDao.deleteByRoleId(roleId);
     }
 
     @Override
     public List<String> getAllMenuId(String roleId, List<String> parentIds) {
-        return null;
+        QueryWrapper<SysMenuRole> queryWrapper = new QueryWrapper<>();
+        queryWrapper.select("menu_id").eq("role_id", roleId).notIn("menu_id", parentIds);
+        List<SysMenuRole> sysMenuRoles = sysMenuRoleDao.selectList(queryWrapper);
+        return sysMenuRoles.stream().map(SysMenuRole::getMenuId).collect(Collectors.toList());
     }
 }
