17507f510db7187312c5a1ed65b5bdb3d6068d27
yanlele <331393627@qq.com>
2020-07-29 22:01:33 +0800

    feat: SysMenuRoleDao

---
+++ mybatis-plus-demo1/src/main/java/com/yanle/mybatis/plus/demo1/system/dao/SysMenuRoleDao.java
@@ -0,0 +1,47 @@
+package com.yanle.mybatis.plus.demo1.system.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yanle.mybatis.plus.demo1.system.entity.SysMenuRole;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SysMenuRoleDao extends BaseMapper<SysMenuRole> {
+    /**
+     * 添加角色和菜单的联系
+     * @param sysMenuRole 角色和菜单的实例
+     * @return 返回值
+     */
+    @Insert("insert into sys_menu_role (`menu_id`, `role_id`) values (#{menuId}, #{roleId})")
+    int addMenuRole(SysMenuRole sysMenuRole);
+
+    /**
+     * 根据角色id删除对应的角色和菜单的联系
+     * @param roleId 角色id
+     * @return 返回值
+     */
+    @Delete("delete from sys_menu_role where role_id = #{roleId}")
+    int deleteByRoleId(@Param("roleId")String roleId);
+
+    /**
+     * 根据角色id查询所有菜单id
+     * @param roleId 角色id
+     * @param parentIds 菜单id
+     * @return 所有菜单id
+     */
+    @Select({
+            "<script>",
+            "select menu_id from sys_menu_role",
+            "where role_id = #{roleId} and menu_id not in",
+            "<foreach collection='parentIds' item='id' open='(' separator=',' close=')'>",
+            "#{id}",
+            "</foreach>",
+            "</script>"
+    })
+    List<String> getAllMenuId(@Param("roleId")String roleId, @Param("parentIds")List<String> parentIds);
+}
