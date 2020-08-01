895f97a82b85c9a85d8f272a877598f79e7168fc
yanlele <331393627@qq.com>
2020-08-01 16:16:03 +0800

    feat: 删除

--- mybatis-plus-study/src/test/java/com/yanle/mybatis/plus/study/DeleteTest.java
+++ mybatis-plus-study/src/test/java/com/yanle/mybatis/plus/study/DeleteTest.java
@@ -1,13 +1,16 @@
 package com.yanle.mybatis.plus.study;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import com.yanle.mybatis.plus.study.dao.UserMapper;
+import com.yanle.mybatis.plus.study.entity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -36,5 +39,22 @@ public class DeleteTest {
         columnMap.put("age", 25);
         int rows = userMapper.deleteByMap(columnMap);
         System.out.println("rows: " + rows);
+    }
+
+    /**
+     * 批量删除
+     */
+    @Test
+    public void deleteByBatchIds() {
+        int rows = userMapper.deleteBatchIds(Arrays.asList(12L, 13L, 14L));
+        System.out.println("rows: " + rows);
+    }
+
+    @Test
+    public void deleteByWrapper() {
+        LambdaQueryWrapper<User> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.eq(User::getAge, 27).or().gt(User::getAge, 41);
+        int rows = userMapper.delete(lambdaQueryWrapper);
+        System.out.println("删除条数： " + rows);
     }
 }
