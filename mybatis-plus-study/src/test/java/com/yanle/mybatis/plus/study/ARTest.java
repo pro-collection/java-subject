41e8b02b7d06d73c2d79b8243780d4422f4e6bbf
yanlele <331393627@qq.com>
2020-08-02 13:27:46 +0800

    feat: AR模式， 插入和查询

---
+++ mybatis-plus-study/src/test/java/com/yanle/mybatis/plus/study/ARTest.java
@@ -0,0 +1,52 @@
+package com.yanle.mybatis.plus.study;
+
+import com.yanle.mybatis.plus.study.entity.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ARTest {
+
+    /**
+     * 插入方法
+     */
+    @Test
+    public void insertTest() {
+        User user = new User();
+        user.setName("长草");
+        user.setAge(24);
+        user.setEmail("zc@123kjsdk.com");
+        user.setManagerId(1088248166370832385L);
+        user.setCreateTime(LocalDateTime.now());
+        boolean insert = user.insert();
+        System.out.println("insert: " + insert);
+    }
+
+    /**
+     * 选择
+     */
+    @Test
+    public void selectById1() {
+        User user = new User();
+        User userSelect = user.selectById(1289793409472389122L);
+        // 返回的是一个新对象
+        System.out.println(userSelect);
+    }
+
+    /**
+     * 选择
+     */
+    @Test
+    public void selectById2() {
+        User user = new User();
+        user.setId(1289793409472389122L);
+        User userSelect = user.selectById();
+        // 返回的是一个新对象
+        System.out.println(userSelect);
+    }
+}
