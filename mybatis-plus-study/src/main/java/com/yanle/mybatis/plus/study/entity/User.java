31041ae81a31274c68713d822b56ed3d17b04976
yanlele <331393627@qq.com>
2020-08-02 14:11:06 +0800

    feat： 批量修改插入

--- mybatis-plus-study/src/main/java/com/yanle/mybatis/plus/study/entity/User.java
+++ mybatis-plus-study/src/main/java/com/yanle/mybatis/plus/study/entity/User.java
@@ -13,7 +13,6 @@ import java.time.LocalDateTime;
 @EqualsAndHashCode(callSuper = true)
 public class User extends Model<User> {
     private static final long serialVersionUID = 3660096825159993280L;
-    @TableId(type = IdType.AUTO)
     private Long id;
     private String name;
     private Integer age;
--- mybatis-plus-study/src/test/java/com/yanle/mybatis/plus/study/ServiceTest.java
+++ mybatis-plus-study/src/test/java/com/yanle/mybatis/plus/study/ServiceTest.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class ServiceTest {
@@ -16,11 +19,32 @@ public class ServiceTest {
     @Autowired
     private UserService userService;
 
+    /**
+     * 查询一个内容
+     */
     @Test
     public void getOneTest() {
         LambdaQueryWrapper<User> lambda = new LambdaQueryWrapper<>();
         // 如果查询结果是多个， 但是没有第二个参数 false, 那么是会报错的。
         User one = userService.getOne(lambda.gt(User::getAge, 25), false);
         System.out.println(one);
+    }
+
+    /**
+     * 批量修改插入
+     */
+    @Test
+    public void Batch() {
+        User user1 = new User();
+        user1.setName("张毅");
+        user1.setAge(28);
+
+        User user2 = new User();
+        user2.setName("张毅2");
+        user2.setAge(28);
+
+        List<User> userList = Arrays.asList(user1, user2);
+        boolean saveBatch = userService.saveBatch(userList);
+        System.out.println("saveBatch ： " + saveBatch);
     }
 }
